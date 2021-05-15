4efed60a7f03c055a186fd8fff564e597bf66576
jomefavourite <jfjomefavourite@gmail.com>
2021-05-15 20:19:21 +0100

    fixed the checkbox problem

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-"use strict";
+'use strict';
 
 //selecting elements
-const form = document.querySelector(".form");
-const todoContainer = document.querySelector(".todo__list");
+const form = document.querySelector('.form');
+const todoContainer = document.querySelector('.todo__list');
 // const todos = document.querySelector(".todo__item");
-const todoInput = document.querySelector(".input");
-const lightSwitch = document.getElementById("light");
-const darkSwitch = document.getElementById("dark");
-const header = document.querySelector(".header-bg");
-const body = document.querySelector("body");
-const itemLeft = document.querySelector(".item__left-value");
+const todoInput = document.querySelector('.input');
+const lightSwitch = document.getElementById('light');
+const darkSwitch = document.getElementById('dark');
+const header = document.querySelector('.header-bg');
+const body = document.querySelector('body');
+const itemLeft = document.querySelector('.item__left-value');
+
+//event listeners
+
+form.addEventListener('submit', todoApp);
+todoContainer.addEventListener('click', deleteCheck);
 
 //state variables
 
@@ -19,29 +24,30 @@ let count = 0;
 function addTodo() {
   //preventing form submittion
 
-  const todoItem = document.createElement("li");
-  todoItem.classList.add("todo__item");
-  const checkbox = document.createElement("div");
-  checkbox.classList.add("icon-box");
-  checkbox.innerHTML = `<svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="check hide"
-        id="check"
-        width="11"
-        height="9"
-      >
-        <path
-          fill="none"
-          stroke="#FFF"
-          stroke-width="2"
-          d="M1 4.304L3.696 7l6-6"
-        />
-      </svg>`;
-  const paragraph = document.createElement("p");
-  paragraph.classList.add("todo__description");
+  const todoItem = document.createElement('li');
+  todoItem.classList.add('todo__item');
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.classList.add('icon-box');
+  // checkbox.innerHTML = `<svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       class="check hide"
+  //       id="check"
+  //       width="11"
+  //       height="9"
+  //     >
+  //       <path
+  //         fill="none"
+  //         stroke="#FFF"
+  //         stroke-width="2"
+  //         d="M1 4.304L3.696 7l6-6"
+  //       />
+  //     </svg>`;
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('todo__description');
   paragraph.innerText = todoInput.value.trim();
-  const deleteBox = document.createElement("div");
-  deleteBox.classList.add("delete-box");
+  const deleteBox = document.createElement('div');
+  deleteBox.classList.add('delete-box');
   deleteBox.innerHTML = `<svg
       id="delete"
       class="delete"
@@ -61,7 +67,7 @@ function addTodo() {
   todoItem.appendChild(deleteBox);
   todoContainer.appendChild(todoItem);
 
-  todoInput.value = "";
+  todoInput.value = '';
 }
 
 function todoApp(e) {
@@ -74,50 +80,49 @@ function deleteCheck(e) {
   const item = e.target;
 
   //delete todo
-  if (item.classList[0] === "delete-box") {
+  if (item.classList[0] === 'delete-box') {
     const todos = item.parentElement;
     count--;
     itemLeft.textContent = count;
     todos.remove();
   }
 
-  if (item.classList[0] === "icon-box") {
+  if (item.classList[0] === 'icon-box') {
     // const checkItem = item.siblingElement;
-    toggle();
+    toggle(item);
   }
 }
-//event listeners
-
-form.addEventListener("submit", todoApp);
-todoContainer.addEventListener("click", deleteCheck);
 
 //THEME SWITCHER
 
 const themeSwicther = function () {
-  lightSwitch.addEventListener("click", () => {
-    header.classList.remove("new-header");
-    lightSwitch.classList.remove("show");
-    darkSwitch.classList.remove("hide");
-    body.classList.remove("dark-theme");
+  lightSwitch.addEventListener('click', () => {
+    header.classList.remove('new-header');
+    lightSwitch.classList.remove('show');
+    darkSwitch.classList.remove('hide');
+    body.classList.remove('dark-theme');
   });
 
-  darkSwitch.addEventListener("click", () => {
-    header.classList.add("new-header");
-    darkSwitch.classList.add("hide");
-    lightSwitch.classList.add("show");
-    body.classList.add("dark-theme");
+  darkSwitch.addEventListener('click', () => {
+    header.classList.add('new-header');
+    darkSwitch.classList.add('hide');
+    lightSwitch.classList.add('show');
+    body.classList.add('dark-theme');
   });
-  localStorage.setItem("theme", themeSwicther);
+  localStorage.setItem('theme', themeSwicther);
 };
 
 //initializes the theme switcher on load of the page
 themeSwicther();
 
-function toggle() {
-  const check = document.querySelector(".icon-box");
-  const checkmark = document.querySelector(".check");
-  const p = document.querySelector(".todo__description");
-  check.classList.toggle("gradient");
-  p.classList.toggle("strike");
-  checkmark.classList.toggle("hide");
+function toggle(item) {
+  // const check = document.querySelector('.icon-box');
+  // const checkmark = document.querySelector(".check");
+  // const p = document.querySelector('.todo__description');
+  // check.classList.toggle("gradient");
+  // p.classList.toggle('strike');
+  // checkmark.classList.toggle("hide");
+
+  item.nextSibling.classList.toggle('strike');
+
 }
